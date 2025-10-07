e7bc128647ded482ec43583b8b2fb386bf1b632f
CORTUNA_1C <cortunajenylyn@mail.com>
2025-10-07 13:50:01 +0800

    summary
    web ngani

--- handsOn1C.js
+++ handsOn1C.js
@@ -1,4 +1,11 @@
 function checkPurchase  (){
+let product = document.getElementById("product").value;
+let message = document.getElementById("product");
+console.log(product)
 
+if(product){
+    console.log("Proceed to next");
+} else if (price <=0 && >=500 );
+console.log
 
 }
